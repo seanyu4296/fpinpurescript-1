8b08c3480557d444549b8153407deb0dab206c38
Sean Que <wyndell@zap.com.ph>
2019-01-07 16:51:18 +0800

    Ex 2.2

--- src/Chapter2.purs
+++ src/Chapter2.purs
@@ -1,10 +1,53 @@
-module Chapter2
-  (formatAbs) where
+module Chapter2 where
 
 import Prelude
+import Data.Array
+import Data.Maybe
 
 abs :: Int -> Int
 abs n = if n < 0 then -n else n
 
 formatAbs :: Int -> String
 formatAbs x = "The absolute value of " <> show x <> " is " <> show (abs x)
+
+factorial :: Int -> Int
+factorial n = go n 1
+  where
+    go :: Int -> Int -> Int
+    go x acc | x <= 0 = acc
+             | otherwise = go (x-1) (x*acc)
+
+formatResult :: String -> Int -> (Int -> Int) -> String
+formatResult name n f = "The " <> name <> " of " <> show n <> " is " <> show (f n)
+
+findFirst ::  Array String -> String -> Int
+findFirst ss key = loop(0)
+  where
+    loop :: Int -> Int
+    loop n | n >= length ss = -1
+           | index ss n == Just key = n
+           | otherwise = loop (n + 1)
+
+polyFindFirst :: forall a. Array a -> (a -> Boolean) -> Int
+polyFindFirst as p = loop(0)
+  where
+    compare :: forall a. (a -> Boolean) -> Maybe a -> Boolean
+    compare f b = case b of
+      Just x -> f x
+      _ -> false
+    loop :: Int -> Int
+    loop n | n >= length as = -1
+           | compare p (index as n) = n
+           | otherwise = loop (n + 1)
+
+isSorted :: forall a. Array a -> (a -> a -> Boolean) -> Boolean
+isSorted as ordered = loop(0)
+  where
+    compare :: forall a. (a -> a -> Boolean) -> Maybe a -> Maybe a -> Boolean
+    compare f (Just b) (Just c) = f b c
+    compare f (Just b) Nothing = true
+    compare _ _ _ = true
+    loop :: Int -> Boolean
+    loop n | n >= length as = true
+           | compare ordered (index as n) (index as (n + 1)) = loop (n + 1)
+           | otherwise = false
--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Chapter2 (formatAbs)
+import Chapter2
 
 import Prelude
 import Effect (Effect)
@@ -8,4 +8,8 @@ import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log (formatAbs (-42))
+  log (formatResult "absolute value" (-42) abs)
+  log (formatResult "factorial" 7 factorial)
+  log (show (findFirst ["a", "b", "c"] "b"))
+  log (show (polyFindFirst [1, 2, 3] (_ == 1)))
+  log (show (isSorted [1, 2, 3, 3] (\a b -> a <= b)))
