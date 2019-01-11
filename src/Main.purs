de42896244eb3caaf08ff2adbaba1f13600fd30e
seanyu4296 <seanyu4296@gmail.com>
2019-01-11 18:47:24 +0800

    Add chapter 5.12 stuff

--- src/Main.purs
+++ src/Main.purs
@@ -4,27 +4,34 @@ import Chapter5
 import Data.Lazy
 import Prelude
 
-import Chapter4 (List(Nil), (:))
+import Data.Tuple
+import Chapter4 (List(Nil), (:), Option(..))
 import Debug.Trace (spy, trace)
 import Effect (Effect)
 import Effect.Console (log, logShow)
 
 main :: Effect Unit
 main = do
-  logShow $ toList $ fromList (1 : 2 : 3 : 4 : 5 : Nil)
-  logShow $ toList $ take (fromList (1 : 2 : 3 : 4 : 5 : Nil)) 3
-  logShow $ toList $ drop (fromList (1 : 2 : 3 : 4 : 5 : Nil)) 3
-  logShow $ toList $ takeWhile (fromList (1 : 2 : 3 : 4 : 5 : Nil)) (_ < 3)
-  logShow $ toList $ takeWhile' (fromList (1 : 2 : 3 : 4 : 5 : Nil)) (_ < 4)
-  logShow $ forAll (fromList (1 : 2 : 3 : 4 : 5 : Nil)) (_ < 7)
-  logShow $ toList $ take ones 10
-  logShow $ exists ones (_ == 1)
-  logShow $ toList $ takeWhile' iStream (_ < 5)
-  log "done"
-  logShow $ headOption' iStream
-  logShow $ toList $ take (mapStream ones (_ + 1)) 10
-  logShow $ toList $ take (filterStream (drop iStream 5) (\x -> x `mod` 2 == 0)) 10
-  logShow $ toList $ take (appendStream iStream iStream) 10
+  logShow $ toList $ take (constant' 10) 5
+  logShow $ toList $ take (from 10) 5
+  logShow $ toList $ take iStream 5
+  logShow $ toList $ take squareds 30
+  logShow $ toList $ take (from' 10) 10
+  logShow $ toList $ take (fibs') 10
+  -- logShow $ toList $ fromList (1 : 2 : 3 : 4 : 5 : Nil)
+  -- logShow $ toList $ take (fromList (1 : 2 : 3 : 4 : 5 : Nil)) 3
+  -- logShow $ toList $ drop (fromList (1 : 2 : 3 : 4 : 5 : Nil)) 3
+  -- logShow $ toList $ takeWhile (fromList (1 : 2 : 3 : 4 : 5 : Nil)) (_ < 3)
+  -- logShow $ toList $ takeWhile' (fromList (1 : 2 : 3 : 4 : 5 : Nil)) (_ < 4)
+  -- logShow $ forAll (fromList (1 : 2 : 3 : 4 : 5 : Nil)) (_ < 7)
+  -- logShow $ toList $ take ones 10
+  -- logShow $ exists ones (_ == 1)
+  -- logShow $ toList $ takeWhile' iStream (_ < 5)
+  -- log "done"
+  -- logShow $ headOption' iStream
+  -- logShow $ toList $ take (mapStream ones (_ + 1)) 10
+  -- logShow $ toList $ take (filterStream (drop iStream 5) (\x -> x `mod` 2 == 0)) 10
+  -- logShow $ toList $ take (appendStream iStream iStream) 10
   -- logShow $ toList $ take (flatMapStream' (drop iStream 5) (\x -> fromList (x : x : Nil))) 10
 
   -- logShow $ toList' (defer (\y -> SEmpty ))
@@ -35,10 +42,10 @@ main = do
     two :: Unit -> Int
     two _ = spy "two" 2
 
-    three :: Unit -> Int
-    three _ = spy "three" 3
+    -- three :: Unit -> Int
+    -- three _ = spy "three" 3
 
-    ss :: Stream Int
-    ss = fromList' $ two : three : Nil
+    -- ss :: Stream Int
+    -- ss = fromList' $ two : three : Nil
 
-    xx = toList $ take ss 2
+    -- xx = toList $ take ss 2
